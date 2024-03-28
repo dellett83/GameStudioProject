712e13a9f61a4157c6090d016792b7999188894c
jtothec67 <jamescomer03@gmail.com>
2024-03-28 13:15:34 +0000

    Cried because I'll have to work out inverse kinematics

--- Assets/James's stuff/Scripts/gunBehaviour.cs
+++ Assets/James's stuff/Scripts/gunBehaviour.cs
@@ -10,6 +10,14 @@ public class gunBehaviour : MonoBehaviour
     public GameObject bullet;
     public GameObject shootFromHere;
 
+    public GameObject leftHold;
+    public GameObject rightHold;
+
+    public Rigidbody leftHandRB;
+    public Rigidbody rightHandRB;
+
+    public float handHoldForce = 100f;
+
     public float maxAmmo;
     public float currentAmmo;
 
@@ -32,10 +40,12 @@ public class gunBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Increases reload timer when reloading
         if (reloading)
         {
             reloadTimer += Time.deltaTime;
             
+            // Resets timer when enough time has passed and fills ammo
             if (reloadTimer > reloadTime)
             {
                 reloading = false;
@@ -44,22 +54,33 @@ public class gunBehaviour : MonoBehaviour
             }
         }
 
+        // Make gun aim horizontally based on camera horizontal rotation
         transform.localRotation = Quaternion.Euler(camera.transform.eulerAngles.x + 10, 0, camera.transform.eulerAngles.z + 10);
 
+        // Shoot ray from gun and put crosshair over where the ray hits
         RaycastHit aimPoint;
         Physics.Raycast(transform.position, transform.forward, out aimPoint, Mathf.Infinity);
 
         crosshair.transform.position = camera.WorldToScreenPoint(aimPoint.point);
 
+        // Shoots bullet is able to shoot
         if (!reloading && currentAmmo > 0 && Input.GetMouseButtonDown(0))
         {
             currentAmmo--;
+            // Creates a bullet pointing in the correct direciton
             Instantiate(bullet, shootFromHere.transform.position, transform.rotation);
         }
 
+        // Reloads automatically and manually
         if (currentAmmo <= 0 || Input.GetKeyDown("r"))
         {
             reloading = true;
         }
+
+        //Vector3 leftForceDirection = (leftHold.transform.position - leftHandRB.transform.position).normalized;
+        //leftHandRB.AddForce(leftForceDirection * handHoldForce * Time.deltaTime, ForceMode.Impulse);
+
+        //Vector3 rightForceDirection = (rightHold.transform.position - rightHandRB.transform.position).normalized;
+        //rightHandRB.AddForce(rightForceDirection * handHoldForce * Time.deltaTime, ForceMode.Impulse);
     }
 }
