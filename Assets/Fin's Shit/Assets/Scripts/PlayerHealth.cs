2678a0e040286940cd1c5f51949f1a00ae7c69f6
James <jamescomer03@gmail.com>
2024-05-15 17:21:00 +0100

    idk

--- Assets/Fin's Shit/Assets/Scripts/PlayerHealth.cs
+++ Assets/Fin's Shit/Assets/Scripts/PlayerHealth.cs
@@ -1,8 +1,9 @@
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour
+public class PlayerHealth : NetworkBehaviour
 {
     public Team teamScript;
     public ragdollPlayerMovement ragdollScript;
@@ -18,11 +19,13 @@ public class PlayerHealth : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (!isLocalPlayer) return;
         health = maxHealth;
     }
 
     void FixedUpdate()
     {
+        if (!isLocalPlayer) return;
         if (regenTimer > healthRegenRate && health < maxHealth)
         {
             health += healthRegenRate;
@@ -34,13 +37,16 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!isLocalPlayer) return;
         regenTimer += Time.deltaTime;
 
         if (health <= 0)
         {
             health = 0;
+            Debug.Log("health is 0");
             ragdollScript.dead = true;
-            GameManager.Instance.PlayerDie(teamScript.teamID); // Maybe use [command]?
+            Die(teamScript.teamID);
+             // Maybe use [command]?
         }
     }
 
@@ -56,8 +62,15 @@ public class PlayerHealth : MonoBehaviour
         if (_bulletsTeam == teamScript.teamID) return;
 
         health -= _damage;
+        Debug.Log(health);
 
         regenTimer = 0;
         // ui also update to show new health
+    }
+
+    [Command]
+    void Die(int _teamID)
+    {
+        GameManager.Instance.PlayerDie(_teamID);
     }
 }
