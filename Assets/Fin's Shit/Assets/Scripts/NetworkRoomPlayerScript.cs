26a89416c224d2ac65bcaa9403dc79a2a92d58c7
Fin Hall (s5509190) <s5509190@bournemouth.ac.uk>
2024-05-16 14:02:46 +0100

    ready up lobby done (for now)

--- Assets/Fin's Shit/Assets/Scripts/NetworkRoomPlayerScript.cs
+++ Assets/Fin's Shit/Assets/Scripts/NetworkRoomPlayerScript.cs
@@ -18,7 +18,16 @@ public class NetworkRoomPlayerScript : NetworkRoomPlayer
 {
 
     public GameObject uiPrefab;
+    public GameObject joinGameIcon;
+    public GameObject canvasPrefab;
+    private static GameObject canvasInstance;
     private GameObject uiInstance;
+
+    [SyncVar]
+    int playersInGame = 0;
+    [SyncVar]
+    int numberOnScreen = 0;
+    //int numberToPrint;
 
     #region Start & Stop Callbacks
 
@@ -29,17 +38,17 @@ public class NetworkRoomPlayerScript : NetworkRoomPlayer
     /// </summary>
     public override void OnStartServer() { }
 
-/// <summary>
-/// Invoked on the server when the object is unspawned
-/// <para>Useful for saving object data in persistent storage</para>
-/// </summary>
-public override void OnStopServer() { }
+    /// <summary>
+    /// Invoked on the server when the object is unspawned
+    /// <para>Useful for saving object data in persistent storage</para>
+    /// </summary>
+    public override void OnStopServer() { }
 
-/// <summary>
-/// Called on every NetworkBehaviour when it is activated on a client.
-/// <para>Objects on the host have this function called, as there is a local client on the host. The values of SyncVars on object are guaranteed to be initialized correctly with the latest state from the server when this function is called on the client.</para>
-/// </summary>
-public override void OnStartClient() 
+    /// <summary>
+    /// Called on every NetworkBehaviour when it is activated on a client.
+    /// <para>Objects on the host have this function called, as there is a local client on the host. The values of SyncVars on object are guaranteed to be initialized correctly with the latest state from the server when this function is called on the client.</para>
+    /// </summary>
+    public override void OnStartClient()
     {
         base.OnStartClient();
 
@@ -61,17 +70,17 @@ public override void OnStartClient()
         }
     }
 
-/// <summary>
-/// This is invoked on clients when the server has caused this object to be destroyed.
-/// <para>This can be used as a hook to invoke effects or do client specific cleanup.</para>
-/// </summary>
-public override void OnStopClient() { }
+    /// <summary>
+    /// This is invoked on clients when the server has caused this object to be destroyed.
+    /// <para>This can be used as a hook to invoke effects or do client specific cleanup.</para>
+    /// </summary>
+    public override void OnStopClient() { }
 
-/// <summary>
-/// Called when the local player object has been set up.
-/// <para>This happens after OnStartClient(), as it is triggered by an ownership message from the server. This is an appropriate place to activate components or functionality that should only be active for the local player, such as cameras and input.</para>
-/// </summary>
-public override void OnStartLocalPlayer() 
+    /// <summary>
+    /// Called when the local player object has been set up.
+    /// <para>This happens after OnStartClient(), as it is triggered by an ownership message from the server. This is an appropriate place to activate components or functionality that should only be active for the local player, such as cameras and input.</para>
+    /// </summary>
+    public override void OnStartLocalPlayer()
     {
         base.OnStartLocalPlayer();
 
@@ -85,30 +94,30 @@ public override void OnStartLocalPlayer()
         }
     }
 
-/// <summary>
-/// This is invoked on behaviours that have authority, based on context and <see cref="NetworkIdentity.hasAuthority">NetworkIdentity.hasAuthority</see>.
-/// <para>This is called after <see cref="OnStartServer">OnStartServer</see> and before <see cref="OnStartClient">OnStartClient.</see></para>
-/// <para>When <see cref="NetworkIdentity.AssignClientAuthority"/> is called on the server, this will be called on the client that owns the object. When an object is spawned with <see cref="NetworkServer.Spawn">NetworkServer.Spawn</see> with a NetworkConnectionToClient parameter included, this will be called on the client that owns the object.</para>
-/// </summary>
-public override void OnStartAuthority() { }
+    /// <summary>
+    /// This is invoked on behaviours that have authority, based on context and <see cref="NetworkIdentity.hasAuthority">NetworkIdentity.hasAuthority</see>.
+    /// <para>This is called after <see cref="OnStartServer">OnStartServer</see> and before <see cref="OnStartClient">OnStartClient.</see></para>
+    /// <para>When <see cref="NetworkIdentity.AssignClientAuthority"/> is called on the server, this will be called on the client that owns the object. When an object is spawned with <see cref="NetworkServer.Spawn">NetworkServer.Spawn</see> with a NetworkConnectionToClient parameter included, this will be called on the client that owns the object.</para>
+    /// </summary>
+    public override void OnStartAuthority() { }
 
-/// <summary>
-/// This is invoked on behaviours when authority is removed.
-/// <para>When NetworkIdentity.RemoveClientAuthority is called on the server, this will be called on the client that owns the object.</para>
-/// </summary>
-public override void OnStopAuthority() { }
+    /// <summary>
+    /// This is invoked on behaviours when authority is removed.
+    /// <para>When NetworkIdentity.RemoveClientAuthority is called on the server, this will be called on the client that owns the object.</para>
+    /// </summary>
+    public override void OnStopAuthority() { }
 
-#endregion
+    #endregion
 
-#region Room Client Callbacks
+    #region Room Client Callbacks
 
-/// <summary>
-/// This is a hook that is invoked on all player objects when entering the room.
-/// <para>Note: isLocalPlayer is not guaranteed to be set until OnStartLocalPlayer is called.</para>
-/// </summary>
-public override void OnClientEnterRoom() 
+    /// <summary>
+    /// This is a hook that is invoked on all player objects when entering the room.
+    /// <para>Note: isLocalPlayer is not guaranteed to be set until OnStartLocalPlayer is called.</para>
+    /// </summary>
+    [Command]
+    public override void OnClientEnterRoom()
     {
-
         base.OnClientEnterRoom();
         Debug.Log("OnClientEnterRoom called.");
 
@@ -119,14 +128,30 @@ public override void OnClientEnterRoom()
             {
                 uiManager.UpdateStatus("You have entered the room.");
             }
+            AddPlayer();
+            Debug.Log("Players in game: " + playersInGame);
+        }
+        Debug.Log("Players in game: " + playersInGame);
+        //numberToPrint = playersInGame - numberOnScreen;
+        for (int numberToPrint = playersInGame - numberOnScreen; numberToPrint > 0; numberToPrint--)
+        {
+            Debug.Log("Number to print: " + numberToPrint);
+            RcpShowJoinGameIcon();
+            numberOnScreen++;
         }
 
     }
 
-/// <summary>
-/// This is a hook that is invoked on all player objects when exiting the room.
-/// </summary>
-public override void OnClientExitRoom() 
+    [ClientCallback]
+    public void AddPlayer()
+    {
+        playersInGame += 1;
+    }
+
+    /// <summary>
+    /// This is a hook that is invoked on all player objects when exiting the room.
+    /// </summary>
+    public override void OnClientExitRoom()
     {
 
         base.OnClientExitRoom();
@@ -145,16 +170,16 @@ public override void OnClientExitRoom()
 
     }
 
-#endregion
+    #endregion
 
-#region SyncVar Hooks
+    #region SyncVar Hooks
 
-/// <summary>
-/// This is a hook that is invoked on clients when the index changes.
-/// </summary>
-/// <param name="oldIndex">The old index value</param>
-/// <param name="newIndex">The new index value</param>
-public override void IndexChanged(int oldIndex, int newIndex) 
+    /// <summary>
+    /// This is a hook that is invoked on clients when the index changes.
+    /// </summary>
+    /// <param name="oldIndex">The old index value</param>
+    /// <param name="newIndex">The new index value</param>
+    public override void IndexChanged(int oldIndex, int newIndex)
     {
 
         base.IndexChanged(oldIndex, newIndex);
@@ -165,19 +190,20 @@ public override void IndexChanged(int oldIndex, int newIndex)
             UIManager uiManager = uiInstance.GetComponent<UIManager>();
             if (uiManager != null)
             {
-                uiManager.UpdateStatus($"Player index changed from {oldIndex} to {newIndex}");
+                //uiManager.UpdateStatus($"Player index changed from {oldIndex} to {newIndex}");
+                uiManager.UpdateStatus("You have entered the room.");
             }
         }
 
     }
 
-/// <summary>
-/// This is a hook that is invoked on clients when a RoomPlayer switches between ready or not ready.
-/// <para>This function is called when the a client player calls SendReadyToBeginMessage() or SendNotReadyToBeginMessage().</para>
-/// </summary>
-/// <param name="oldReadyState">The old readyState value</param>
-/// <param name="newReadyState">The new readyState value</param>
-public override void ReadyStateChanged(bool oldReadyState, bool newReadyState) 
+    /// <summary>
+    /// This is a hook that is invoked on clients when a RoomPlayer switches between ready or not ready.
+    /// <para>This function is called when the a client player calls SendReadyToBeginMessage() or SendNotReadyToBeginMessage().</para>
+    /// </summary>
+    /// <param name="oldReadyState">The old readyState value</param>
+    /// <param name="newReadyState">The new readyState value</param>
+    public override void ReadyStateChanged(bool oldReadyState, bool newReadyState)
     {
 
         base.ReadyStateChanged(oldReadyState, newReadyState);
@@ -194,14 +220,52 @@ public override void ReadyStateChanged(bool oldReadyState, bool newReadyState)
 
     }
 
-#endregion
+    #endregion
 
-#region Optional UI
+    #region ClientRpc Methods
 
-//public override void OnGUI()
-//{
-//    base.OnGUI();
-//}
+    //[ClientRpc]
+    void RcpShowJoinGameIcon()
+    {
+        Debug.Log("RpcShowJoinGameIcon called.");
+
+        // Ensure there is only one canvas
+        if (canvasInstance == null)
+        {
+            // Instantiate the canvas prefab if it doesn't exist
+            if (canvasPrefab != null)
+            {
+                canvasInstance = Instantiate(canvasPrefab);
+            }
+            else
+            {
+                Debug.LogError("canvasPrefab is not assigned!");
+                return;
+            }
+        }
+
+        // Null check for joinGameIcon before attempting to instantiate it
+        if (joinGameIcon != null)
+        {
+            // Instantiate the join game icon and set its parent to the canvas
+            GameObject indicator = Instantiate(joinGameIcon);
+            indicator.transform.SetParent(canvasInstance.transform, false);
+        }
+        else
+        {
+            Debug.LogError("joinGameIcon is not assigned!");
+        }
+    }
+
+
+    #endregion
+
+    #region Optional UI
+
+    //public override void OnGUI()
+    //{
+    //    base.OnGUI();
+    //}
 
     #endregion
 }
